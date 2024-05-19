8fd56c39d6b8822678d8a04e0e90770f0e58298e
akshil2908 <akkisutariya2908@gmail.com>
2024-05-19 21:52:48 +0530

    chnage

--- main/settings.py
+++ main/settings.py
@@ -7,17 +7,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from distutils.command.build import build
 from pathlib import Path
-import os
 import mimetypes
 
 mimetypes.add_type("text/javascript", ".js", True)
 mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +25,7 @@ SECRET_KEY = 'dr&-v@o7w9#&#r3wj$d#$t78t&*hb$&(2)xa5@05d1p$)1=$96'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = ['*']
 
 STRIPE_PUB_KEY = 'pk_test_51Ikdb4HwFojfx57irEolkyNAWZCsGCRlrBEKIt4WZXhOTg25Lw50WpUFY5OmDb504spl1XcnaQlswKQu0ULBkpbD003XV2jNJt'
 STRIPE_SECRET_KEY = 'sk_test_51Ikdb4HwFojfx57ik70ouFvnOA2MWIE2QqZkcCmB3yO142YvuuLWc4pSBkI5yDT4y77a9oDq6NWWYXxNtGn52A3700yNZd411N'
@@ -40,7 +38,6 @@ SESSION_COOKIE_AGE = 86400
 CART_SESSION_ID = 'cart'
 COUPON_SESSION_ID = 'coupon'
 SESSION_SAVE_EVERY_REQUEST = True
-
 
 # Application definition
 
@@ -73,7 +70,6 @@ INSTALLED_APPS = [
     'apps.ordering',
     'apps.maintenance',
 
-
     # 'rest_framework',
     'widget_tweaks',
     'crispy_forms',
@@ -85,8 +81,6 @@ INSTALLED_APPS = [
     'storages',
     "collectfast",
     'django_ses',
-
-
 ]
 
 MIDDLEWARE = [
@@ -101,7 +95,6 @@ MIDDLEWARE = [
     'htmlmin.middleware.HtmlMinifyMiddleware',
     'htmlmin.middleware.MarkRequestMiddleware',
 ]
-
 
 ROOT_URLCONF = 'main.urls'
 
@@ -128,17 +121,15 @@ UNDER_CONSTRUCTION = False
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -157,7 +148,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -179,7 +169,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
 EMAIL_BACKEND = 'django_ses.SESBackend'
 DEFAULT_EMAIL_FROM = 'customerservice@sokopark.com'
 SERVER_EMAIL = 'customerservice@sokopark.com'
@@ -187,27 +176,24 @@ SERVER_EMAIL = 'customerservice@sokopark.com'
 AWS_SES_REGION_NAME = 'eu-west-1'
 AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
 
-
 AWS_ACCESS_KEY_ID = 'AKIA2K3OSTWJYNYQ6Q5W'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'sokopark'
 AWS_S3_REGION_NAME = 'af-south-1'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.af-south-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.af-south-1.amazonaws.com'
 AWS_PRELOAD_METADATA = True
 AWS_DEFAULT_ACL = None
 AWS_S3_SECURE_URLS = True
 AWS_EXPIRY = 60 * 60 * 24 * 7
 AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age={}, s-maxage={}, must-revalidate'.format(AWS_EXPIRY, AWS_EXPIRY)
+    'CacheControl': f'max-age={AWS_EXPIRY}, s-maxage={AWS_EXPIRY}, must-revalidate'
 }
 AWS_LOCATION = 'static'
 AWS_IS_GZIPPED = True
-# COLLECTFAST_THREADS = 20
 
-
-COMPRESS_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+COMPRESS_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 if not DEBUG:
     STATICFILES_LOCATION = 'static'
@@ -215,30 +201,28 @@ if not DEBUG:
     STATICFILES_STORAGE = 'main.s3utils.CachedS3Boto3Storage'
     COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
     COMPRESS_STORAGE = STATICFILES_STORAGE
-    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     COMPRESS_URL = STATIC_URL
 else:
     STATIC_URL = '/static/'
     MEDIA_URL = '/media/'
 
-
-# STATIC_ROOT = "/home/ubuntu/static/"
 COMPRESS_ENABLED = True
 COMPRESS_CSS_HASHING_METHOD = 'content'
-COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',
-                        'compressor.filters.cssmin.CSSMinFilter']
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter'
+]
 COMPRESS_JS_FILTERS = ['compressor.filters.jsmin.JSMinFilter']
 COMPRESS_PARSER = 'compressor.parser.HtmlParser'
 HTML_MINIFY = True
 KEEP_COMMENTS_ON_MINIFYING = False
 
-
-STATICFILES_FINDERS = (
+STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # other finders..
     'compressor.finders.CompressorFinder',
-)
+]
 
 GZIP_CONTENT_TYPES = (
     'text/css',
@@ -247,12 +231,7 @@ GZIP_CONTENT_TYPES = (
     'text/javascript'
 )
 
-
 CKEDITOR_UPLOAD_PATH = 'images/'
 CKEDITOR_ALLOW_NONIMAGE_FILES = False
-
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
 CKEDITOR_IMAGE_BACKEND = "pillow"
-
-
-# SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
